ce9f68a266e36e9eb7f78adfb85ab782237edced
MiltonB1 <milton1villa23@gmail.com>
2023-10-06 13:00:07 -0300

    Se agrego el envio por gmail
    Al admin le llegara los mensajes por Gmail

--- src/contacto/contacto.service.ts
+++ src/contacto/contacto.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Contacto } from './entities/contacto.entity';
 import { Repository } from 'typeorm';
+import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class ContactoService {
@@ -18,6 +19,26 @@ export class ContactoService {
       const contacto = this.contactoRepository.create(contactoData);
       //Guardado del contacto en nuestra base de datos.
       const nuevoContacto = await this.contactoRepository.save(contacto);
+
+      //Se hace envio al correo de la Pyme
+      const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+          user: 'flowtech2023a@gmail.com',
+          pass: 'svqo qhgm dgcd ynyb',
+        },
+      });
+
+      const gmail = {
+        from: 'flowtech2023a@gmail.com',
+        to: 'flowtech2023a@gmail.com',
+        subject: 'Servicio',
+        text: `${contactoData.nombre_mensaje} 
+        ${contactoData.apellido_mensaje},${contactoData.email_mensaje},${contactoData.telefono_mensaje} 
+        . Email:${contactoData.mensaje_mensaje}`,
+      };
+
+      await transporter.sendMail(gmail);
       return nuevoContacto;
     } catch (error) {
       //En caso de error manda un mensaje en la consola.
