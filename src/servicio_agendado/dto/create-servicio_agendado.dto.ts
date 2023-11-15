545e845db4a9cf208e04d71f51e9c51bc5c6403b
David Villalobos <davi.villalobos@duocuc.cl>
2023-11-15 13:07:53 -0300

    cambios bd

--- src/servicio_agendado/dto/create-servicio_agendado.dto.ts
+++ src/servicio_agendado/dto/create-servicio_agendado.dto.ts
@@ -2,29 +2,22 @@ import { IsString, IsEmail, IsNumber, IsNotEmpty, IsEnum } from 'class-validator
 
 export class CreateServicioAgendadoDto {
   @IsNotEmpty()
-  @IsString()
   comuna: string;
 
   @IsNotEmpty()
-  @IsString()
   direccion: string;
 
   @IsNotEmpty()
-  @IsEmail()
   email: string;
 
   @IsNotEmpty()
-  @IsString()
   revision_tecnica: string;
 
   @IsNotEmpty()
-  @IsNumber()
   servicioId: number;
 
   @IsNotEmpty()
-  @IsNumber()
   telefono: number;
 
-  @IsNotEmpty()
   estado_servicio: string;
 }
